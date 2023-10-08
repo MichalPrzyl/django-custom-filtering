e0e2d961d6ad0912ea5cca06da2441c45a0e1984
MichalPrzyl <michal.przyl@gmail.com>
2023-10-08 16:27:50 +0200

    Add django-filters to project and specifically for article view

--- backend/article/views.py
+++ backend/article/views.py
@@ -3,8 +3,12 @@ from rest_framework import generics
 from article.models import Article
 # serializers
 from article.serializers import ArticleSerializer
+# filter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ArticleAPI(generics.ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'
