de135f6c52b41417a67da05d8fe56b2bfb00f4bf
MichalPrzyl <michal.przyl@gmail.com>
2023-10-09 00:16:05 +0200

    Columns in django-admin

--- backend/author/admin.py
+++ backend/author/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Author, AuthorCategory, Tag
 
 
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name']
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(AuthorCategory)
 admin.site.register(Tag)
 
